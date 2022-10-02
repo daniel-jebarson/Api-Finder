853661dd41f97cdd80a44fcb8e171efdb57ab40e
daniel-jebarson <danieljebarson21@gmail.com>
2022-10-02 21:44:54 +0530

    Added redirection to API websites 👻

--- components/Card.js
+++ components/Card.js
@@ -1,10 +1,15 @@
-import { Image } from "@chakra-ui/react";
-import { useState } from "react";
+import Router from "next/router";
 function Card(props) {
   const { data } = props;
 
   return (
-    <div className="flex flex-col w-1/5 max-w-md rounded-lg p-2 min-h-fit border border-gray items-center hover:cursor-pointer my-1 transition-all animate-none [transition-duration:200ms] shadow-md hover:[transform:scale(1.075)] justify-center bg-gray-light">
+    <div
+      onClick={() => {
+        Router.push(data.Link);
+        console.log("Clicked " + data.Link);
+      }}
+      className="flex flex-col w-1/5 max-w-md rounded-lg p-2 min-h-fit border border-gray items-center hover:cursor-pointer my-1 transition-all animate-none [transition-duration:200ms] shadow-md hover:[transform:scale(1.075)] justify-center bg-gray-light"
+    >
       <div></div>
       <h1 className="text-xl font-semibold p-1.5 text-center">{data.API}</h1>
 
